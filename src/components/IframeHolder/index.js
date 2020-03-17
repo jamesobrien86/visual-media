a2e574ae118403ee5d806837376578a68bb55b6d
James O'Brien <jamesobrien.web@gmail.com>
2020-03-17 11:13:07 +0300

    impl - ifrma size

--- src/components/IframeHolder/index.js
+++ src/components/IframeHolder/index.js
@@ -47,7 +47,7 @@ export default function IframeHolder(props){
             />
             ) : null}
             <div className={`frame-vis ${loading ? "loading-frame":"loaded"}`} >
-                <iframe id={props.title} onLoad={() => hideSpinner()} width="100%" height="95%" src={props.url} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={false}></iframe>
+                <iframe id={props.title} onLoad={() => hideSpinner()} width="100%" height="100%" src={props.url} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen={false}></iframe>
             </div>
         </div>
         </div>
